75185d499a5d1c5c740805e04d3172471a5091be
suhad shaheen <s12325463@stu.najah.edu>
2026-03-03 22:47:14 +0200

    assignment1

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
 export default function SearchShell() {
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView
-        source={{ uri: "https://www.google.com" }}
-        style={styles.webview}
-      />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: "https://www.google.com" }}
+          style={styles.webview}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
