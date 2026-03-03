b7b1a2e48cba551b30f64f84aaf3fa6aa1fbb41c
suhad shaheen <s12325463@stu.najah.edu>
2026-03-03 18:52:46 +0200

    Initial commit - assignment1

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
+
+export default function SearchShell() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: "https://www.google.com" }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
